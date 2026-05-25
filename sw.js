07408456eb22ff718fdd8c7f8deff97c0e1fd8b6
tschts012026 <tschts01@gmail.com>
2026-05-25 15:38:43 +0800

    v21: add PWA offline support (manifest + sw)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'wfm-v7';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'wfm-v21';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
